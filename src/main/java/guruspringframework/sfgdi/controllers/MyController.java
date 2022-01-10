fce8d1b1fa5452d921224467ceb6f5d48f89f164
Ali Zhao <0604aliz@gmail.com>
2022-01-10 18:34:21 -0500

    add primary beans

--- src/main/java/guruspringframework/sfgdi/controllers/MyController.java
+++ src/main/java/guruspringframework/sfgdi/controllers/MyController.java
@@ -1,11 +1,18 @@
 package guruspringframework.sfgdi.controllers;
 
+import guruspringframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello word!!!");
-        return "Hi focks!";
+        return greetingService.sayGreeting();
     }
 }
