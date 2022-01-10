20767966f89d0679107179ab4f4a7e61bb5a71f6
Ali Zhao <0604aliz@gmail.com>
2022-01-10 13:21:44 -0500

    add dependency injection

--- src/main/java/guruspringframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guruspringframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guruspringframework.sfgdi;
 
+import guruspringframework.sfgdi.controllers.ConstructorInjectedController;
 import guruspringframework.sfgdi.controllers.MyController;
+import guruspringframework.sfgdi.controllers.PropertyInjectedController;
+import guruspringframework.sfgdi.controllers.SetterInjectedController;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +20,18 @@ public class SfgDiApplication {
 
 		String greeting=myController.sayHello();
 		System.out.println(greeting);
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController=(PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("------ Setter");
+		SetterInjectedController setterInjectedController=(SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("------ Constructor");
+		ConstructorInjectedController constructorInjectedController=(ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
--- src/main/java/guruspringframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guruspringframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,10 @@
 package guruspringframework.sfgdi.controllers;
 
 import guruspringframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
